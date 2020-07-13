889acd6c9b6b79fa2d46c8c68f7ceb0762bd6315
Lucas <lrs.rafael@hotmail.com>
2020-07-13 19:49:52 -0300

    Validação da aplicação

---
+++ src/FormValidator.js
@@ -0,0 +1,22 @@
+import validador from 'validator';
+
+class FormValidator{
+
+    constructor(validacao){
+        this.validacao = validacao;
+    }
+
+    valida(state){
+        const campoValor = state[this.validacao.campo.toString()];
+        const metodoValidacao = validador[this.validacao.metodo];
+
+        if(metodoValidacao(campoValor, [], state) === true){
+            console.log("FORM INVALIDO");
+            return false;
+        }else{
+            console.log("FORM VÁLIDO");
+            return true;
+        }
+    }
+}
+export default FormValidator;
